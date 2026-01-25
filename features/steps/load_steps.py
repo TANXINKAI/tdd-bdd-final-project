872f53aea8d043d8cb09f00c801e643625618698
TANXINKAI <tan0xin0kai@gmail.com>
2026-01-24 23:56:24 -0500

    fix data bug

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -51,9 +51,8 @@ def step_impl(context):
             "name": row['name'],
             "description": row['description'],
             "price": row['price'],
-            "available": row['available'],
+            "available": row['available'] in ['True', 'true', '1'],
             "category": row['category']
         }
-        
         context.resp = requests.post(rest_endpoint, json=payload)
-        assert(context.resp.status_code == HTTP_201_CREATED)
+        assert context.resp.status_code == HTTP_201_CREATED
